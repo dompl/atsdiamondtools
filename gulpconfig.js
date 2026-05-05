a203d00822b3c6d5f74f593d20480afe65b4c7ca
rfsdev server <ino@redfrogstudio.co.uk>
2026-05-05 15:10:44 +0000

    Increment theme version to 0.0.73

--- gulpconfig.js
+++ gulpconfig.js
@@ -4,7 +4,7 @@ module.exports = {
     "description": "ATS Diamond Tools WordPress Theme",
     "name": "ATS Diamond Tools WordPress Theme",
     "url": "http://skyline.rfsdev.co.uk",
-    "version": "0.0.71"
+    "version": "0.0.73"
   },
   "project": {
     "name": "atsdiamondtools",
